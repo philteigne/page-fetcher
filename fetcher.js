8ab12cd8e4b27d3c787861ff7731e292e2841dc0
Phil <philippe.teigne@gmail.com>
2023-11-06 20:53:24 -0500

    contain actions within functions and layer them to correctly fire

--- fetcher.js
+++ fetcher.js
@@ -5,65 +5,58 @@ const request = require('request');
 const hostName = process.argv[2];
 const saveLocation = process.argv[3];
 
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdin
-});
-
-fs.access(saveLocation, (err) => {
-  if (!err) {
-    console.log(`${saveLocation} already exists. Would you like to overwrite it? Y/N`);
-    setTimeout(() => {
-      console.log('No response, exiting process');
-      process.exit(2);
-    }, 15000);
-    rl.on('line', (input) => {
-      if (input === 'y' || input === 'Y') {
-        console.log('Overwriting');
-      }
-      if (input === 'n' || input === 'N') {
-        console.log('Process cancelled');
-        process.exit(2);
-      }
-    });
-  }
-});
-
-request(hostName, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    process.exit(2);
-  }
-  console.log('statusCode: ', response && response.statusCode);
-
-  fs.access(saveLocation, (err) => {
-    if (!err) {
-      console.log(`${saveLocation} already exists. Would you like to overwrite it? Y/N`);
-      setTimeout(() => {
-        console.log('No response, exiting process');
-        process.exit(2);
-      }, 15000);
-      rl.on('line', (input) => {
-        if (input === 'y' || input === 'Y') {
-          console.log('Overwriting');
-        }
-        if (input === 'n' || input === 'N') {
-          console.log('Process cancelled');
-          process.exit(2);
-        }
-      });
-    }
-  });
-  
+const writeFile = (body) => {
   fs.writeFile(saveLocation, body, (err) => {
     if (err) {
+      if (err.errno === -13) {
+        console.log('Error: Invalid path');
+        return;
+      }
       console.log(err);
     } else {
       console.log("File written successfully");
-      process.exit(2);
+      return;
     }
   });
-});
+};
 
-// http://www.example.edu/ ./index.html
+const fetchRequest = () => {
+  request(hostName, (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    } else {
+      fs.access(saveLocation, (err) => {
+        if (!err) {
+          console.log(`${saveLocation} already exists. Would you like to overwrite it? Y/N`);
+
+          const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdin
+          });
+
+          rl.on('line', (input) => {
+            if (input === 'y' || input === 'Y') {
+              console.log('Overwriting');
+              writeFile(body);
+            }
+            if (input === 'n' || input === 'N') {
+              console.log('Process cancelled');
+            }
+            rl.close();
+          });
+
+        } else {
+          writeFile(body);
+        }
+      });
+      
+    }
+  });
+};
+
+if (!hostName || !saveLocation) {
+  console.log('Please enter a URL and location');
+} else {
+  fetchRequest();
+}
